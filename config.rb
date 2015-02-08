c560f8deaa90a0b1a20448f71c30cd7917bf0f92
Sonny Gonzalez <sg@essgee.io>
2015-02-07 23:03:41 -0600

    more better foundation wiring

--- config.rb
+++ config.rb
@@ -1,14 +1,24 @@
+require 'awesome_print'
+
 set  :css_dir,        'stylesheets'
 set  :js_dir,         'javascripts'
 set  :images_dir,     'images'
 set  :fonts_dir,      'fonts'
 
+sprockets.append_path File.join root, 'foundation/js/foundation'
+sprockets.append_path File.join root, 'foundation/scss'
+sprockets.append_path File.join root, 'foundation/scss/foundation/components'
+
 sprockets.append_path 'javascripts/lib'
 sprockets.append_path 'javascripts/vendor'
+
+sprockets.append_path 'stylesheets/lib'
+ap sprockets.append_path 'stylesheets/vendor'
 
 activate   :directory_indexes
 
 configure :development do
+  # set      :debug_assets, true
   activate :livereload
 end
 
