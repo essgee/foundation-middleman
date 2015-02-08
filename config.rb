1dbee227a1e558a31c5eb0f18797260bd83c618c
Sonny Gonzalez <sg@essgee.io>
2015-02-07 23:20:33 -0600

    foundation working out of local repo

--- config.rb
+++ config.rb
@@ -5,15 +5,16 @@ set  :js_dir,         'javascripts'
 set  :images_dir,     'images'
 set  :fonts_dir,      'fonts'
 
-sprockets.append_path File.join root, 'foundation/js/foundation'
-sprockets.append_path File.join root, 'foundation/scss'
-sprockets.append_path File.join root, 'foundation/scss/foundation/components'
-
 sprockets.append_path 'javascripts/lib'
 sprockets.append_path 'javascripts/vendor'
 
 sprockets.append_path 'stylesheets/lib'
-ap sprockets.append_path 'stylesheets/vendor'
+sprockets.append_path 'stylesheets/vendor'
+
+sprockets.append_path File.join root, 'foundation/js/foundation'
+asset_paths = sprockets.append_path File.join root, 'foundation/scss'
+
+ap asset_paths
 
 activate   :directory_indexes
 
